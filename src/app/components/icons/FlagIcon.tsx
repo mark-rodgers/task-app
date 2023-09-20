de603e6ded0e73aed5291bacdb13135558fe157b
Mark Rodgers <mark@marknrodgers.com>
2023-09-19 18:10:04 -0700

    add expand, flag, and tag icons

---
+++ src/app/components/icons/FlagIcon.tsx
@@ -0,0 +1,19 @@
+export default function FlagIcon(props: any) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M12.439 7l2.4-3H7v6h7.839l-2.4-3zM19 12H7v10H5V2h14l-4 5 4 5z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
