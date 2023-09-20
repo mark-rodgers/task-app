97f3ddb2357725697196e461bb25b3715c728307
Mark Rodgers <mark@marknrodgers.com>
2023-09-19 18:18:05 -0700

    add trash icon

--- src/app/components/icons/TrashIcon.tsx
+++ src/app/components/icons/TrashIcon.tsx
@@ -1,4 +1,4 @@
-export default function CommentIcon(props: any) {
+export default function TrashIcon(props: any) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -8,13 +8,13 @@ export default function CommentIcon(props: any) {
       viewBox="0 0 24 24"
       {...props}
     >
-      <path fill="currentColor" d="M17 9H7V7h10v2zM7 13h10v-2H7v2z"></path>
       <path
         fill="currentColor"
         fillRule="evenodd"
-        d="M2 18V2h20v16h-6v4h-2a4 4 0 01-4-4H2zm10-2v2a2 2 0 002 2v-4h6V4H4v12h8z"
+        d="M17 5V4a2 2 0 00-2-2H9a2 2 0 00-2 2v1H4a1 1 0 000 2h1v11a3 3 0 003 3h8a3 3 0 003-3V7h1a1 1 0 100-2h-3zm-2-1H9v1h6V4zm2 3H7v11a1 1 0 001 1h8a1 1 0 001-1V7z"
         clipRule="evenodd"
       ></path>
+      <path fill="currentColor" d="M9 9h2v8H9V9zM13 9h2v8h-2V9z"></path>
     </svg>
   );
 }
