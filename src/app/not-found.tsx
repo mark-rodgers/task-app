aadd3a5ae05797ea37dd3acabc982225aef022c7
Mark Rodgers <mark@marknrodgers.com>
2023-09-19 22:21:15 -0700

    404 updates

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,20 @@
+import { Tomorrow } from "next/font/google";
 import Image from "next/image";
 import Button from "@/app/_components/Button";
 
+const tomorrow = Tomorrow({ weight: ["400"], subsets: ["latin"] });
+
 export default function NotFound() {
   return (
-    <div className="flex w-full flex-col items-center justify-center bg-[#040205] p-4">
+    <div
+      className={`${tomorrow.className} flex w-full flex-col items-center justify-center bg-[#040205] p-4`}
+    >
       <Image src="/404.gif" alt="Page not found" width={350} height={350} />
       <Button
         href="/"
         className="rounded-none border-white bg-black text-white hover:border-white hover:bg-white hover:text-black focus:border-white focus:bg-white focus:text-black"
       >
-        Go Back
+        GO BACK_
       </Button>
     </div>
   );
