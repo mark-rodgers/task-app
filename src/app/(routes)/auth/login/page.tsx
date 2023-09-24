45acab499027cd14ae56c1370a28147896a52995
Mark Rodgers <mark@marknrodgers.com>
2023-09-24 00:33:48 -0700

    add signup form and Micrsoft SSO button

--- src/app/(routes)/auth/login/page.tsx
+++ src/app/(routes)/auth/login/page.tsx
@@ -5,9 +5,10 @@ import Image from "next/image";
 import { redirect } from "next/navigation";
 import { signIn } from "next-auth/react";
 import { useSession } from "next-auth/react";
-// import { Transition } from "@headlessui/react";
+import { Transition } from "@headlessui/react";
 import TextBox from "@/app/_components/TextBox";
 import Button from "@/app/_components/Button";
+import ChevronRightIcon from "@/app/_components/icons/ChevronRightIcon";
 
 function handleLoginFormSubmit(e: React.MouseEvent<HTMLElement>) {
   e.preventDefault();
@@ -16,26 +17,24 @@ function handleLoginFormSubmit(e: React.MouseEvent<HTMLElement>) {
   );
 }
 
-function handleRegisterFormSubmit(e: React.MouseEvent<HTMLButtonElement>) {
+function handleRegisterFormSubmit(e: React.MouseEvent<HTMLElement>) {
   e.preventDefault();
+  alert(
+    "Email logins are not implemented yet. Please sign in with your Google or GitHub account.",
+  );
+}
+
+function handleMicrosoftLogin(e: React.MouseEvent<HTMLElement>) {
+  e.preventDefault();
+  alert("Microsoft logins are not implemented yet.");
 }
 
 export default function Login() {
   function toggleCurrentForm(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
-    // if (currentForm === "login") {
-    //   setCurrentForm("register");
-    // } else if (currentForm === "register") {
-    //   setCurrentForm("login");
-    // }
-    switch (currentForm) {
-      case "login":
-        setCurrentForm("register");
-        break;
-      case "register":
-        setCurrentForm("login");
-        break;
-    }
+    currentForm === "login"
+      ? setCurrentForm("register")
+      : setCurrentForm("login");
   }
 
   const { data: session, status } = useSession();
@@ -46,7 +45,7 @@ export default function Login() {
   return (
     <div className="w-full">
       <div className="backgroundImage h-full bg-[url('/bg-login-page.webp')] bg-cover bg-center">
-        <div className="backgroundOverlay flex h-full w-full justify-normal bg-amber-400/50 backdrop-brightness-50 md:justify-center">
+        <div className="backgroundOverlay flex h-full w-full justify-normal overflow-scroll bg-amber-400/50 backdrop-brightness-50 md:justify-center">
           <div className="leftSide m-16 hidden w-fit grow lg:block">
             <div className="flex flex-col pt-16 font-bold lg:text-[5vw] xl:text-[8vw] ">
               <div className="animate-fade-in-left leading-none text-amber-200">
@@ -57,73 +56,156 @@ export default function Login() {
               </div>
             </div>
           </div>
-          <div className="rightSide m-8 h-fit w-full sm:m-16 md:w-fit md:min-w-[600px] lg:ml-0">
-            <div className="flex flex-col rounded-3xl bg-white p-8 sm:p-16">
-              <div className="mb-8 text-4xl font-bold">Sign In</div>
-              <form className="flex flex-col gap-3">
-                <label htmlFor="email" className="font-bold">
-                  Email
-                </label>
-                <TextBox
-                  id="email"
-                  name="email"
-                  placeholder="jonsnow@thewall.com"
-                  className="border p-4 font-normal"
-                />
-                <label htmlFor="password" className="font-bold">
-                  Password
-                </label>
-                <TextBox
-                  type="password"
-                  id="password"
-                  name="password"
-                  className="border p-4 font-normal"
-                />
-                <Button onClick={handleLoginFormSubmit} className="my-5 p-4">
-                  Sign In
-                </Button>
-                <div className="text-center">
-                  Don&apos;t have an account?{" "}
-                  <button
-                    onClick={toggleCurrentForm}
-                    className="bold text-amber-700 hover:underline focus:underline focus:outline-none"
-                  >
-                    Sign up
-                  </button>
-                </div>
-              </form>
-              <div className="relative my-5 flex items-center">
-                <div className="flex-grow border border-t border-stone-200"></div>
-                <span className="mx-4 flex-shrink text-stone-400">or</span>
-                <div className="flex-grow border border-t border-stone-200"></div>
+          <div className="rightSide m-0 h-fit w-full sm:m-16 md:w-fit md:min-w-[600px] lg:ml-0">
+            <div className="relative">
+              <div className="absolute w-full">
+                <Transition
+                  appear={true}
+                  show={currentForm === "login"}
+                  enter="transition-opacity duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="transition-opacity duration-300"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="flex h-screen flex-col overflow-scroll bg-white p-8 sm:h-fit sm:rounded-3xl sm:p-16">
+                    <div className="mb-8 text-4xl font-bold">Sign In</div>
+                    <form className="flex flex-col gap-3">
+                      <label htmlFor="email" className="font-bold">
+                        Email
+                      </label>
+                      <TextBox
+                        id="email"
+                        name="email"
+                        placeholder="jonsnow@thewall.com"
+                        className="border p-4 font-normal"
+                      />
+                      <label htmlFor="password" className="font-bold">
+                        Password
+                      </label>
+                      <TextBox
+                        type="password"
+                        id="password"
+                        name="password"
+                        className="border p-4 font-normal"
+                      />
+                      <Button
+                        onClick={handleLoginFormSubmit}
+                        className="my-5 p-4"
+                      >
+                        Sign In
+                      </Button>
+                      <div className="text-center">
+                        Don&apos;t have an account?{" "}
+                        <button
+                          onClick={toggleCurrentForm}
+                          className="bold text-amber-700 hover:underline focus:underline focus:outline-none"
+                        >
+                          Sign up
+                        </button>
+                      </div>
+                    </form>
+                    <div className="relative my-5 flex items-center">
+                      <div className="flex-grow border border-t border-stone-200"></div>
+                      <span className="mx-4 flex-shrink text-stone-400">
+                        or
+                      </span>
+                      <div className="flex-grow border border-t border-stone-200"></div>
+                    </div>
+                    <div className="flex flex-wrap justify-center gap-3 p-4">
+                      <button
+                        className="w-full rounded-full border border-stone-200 px-8 py-3 font-bold text-stone-700 outline-none transition-colors hover:bg-stone-100 focus:bg-stone-100"
+                        onClick={() => signIn("google")}
+                      >
+                        <Image
+                          src="/logo-google.svg"
+                          alt="Google"
+                          width={24}
+                          height={24}
+                          className="mr-4 inline-block"
+                        />
+                        Sign In with Google
+                      </button>
+                      <button
+                        className="w-full rounded-full border border-stone-200 px-8 py-3 font-bold text-stone-700 outline-none transition-colors hover:bg-stone-100 focus:bg-stone-100"
+                        onClick={(e) => handleMicrosoftLogin(e)}
+                      >
+                        <Image
+                          src="/logo-microsoft.svg"
+                          alt="Microsoft"
+                          width={24}
+                          height={24}
+                          className="mr-4 inline-block"
+                        />
+                        Sign In with Microsoft
+                      </button>
+                      <button
+                        className="w-full rounded-full border border-stone-200 px-8 py-3 font-bold text-stone-700 outline-none transition-colors hover:bg-stone-100 focus:bg-stone-100"
+                        onClick={() => signIn("github")}
+                      >
+                        <Image
+                          src="/logo-github.svg"
+                          alt="GitHub"
+                          width={24}
+                          height={24}
+                          className="mr-4 inline-block"
+                        />
+                        Sign In with GitHub
+                      </button>
+                    </div>
+                  </div>
+                </Transition>
               </div>
-              <div className="flex flex-wrap justify-center gap-3 p-4">
-                <button
-                  className="rounded-full border border-stone-200 px-8 py-3 font-bold text-stone-700 outline-none transition-colors hover:bg-stone-100 focus:bg-stone-100"
-                  onClick={() => signIn("google")}
+              <div className="absolute w-full">
+                <Transition
+                  appear={true}
+                  show={currentForm === "register"}
+                  enter="transition-opacity duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="transition-opacity duration-300"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
                 >
-                  <Image
-                    src="/logo-google.svg"
-                    alt="Google"
-                    width={24}
-                    height={24}
-                    className="mr-4 inline-block"
-                  />
-                  Sign In with Google
-                </button>
-                <button
-                  className="rounded-full border border-stone-200 px-8 py-2 font-bold text-stone-700 outline-none transition-colors hover:bg-stone-100 focus:bg-stone-100"
-                  onClick={() => signIn("github")}
-                >
-                  <Image
-                    src="/logo-github.svg"
-                    alt="GitHub"
-                    width={24}
-                    height={24}
-                    className="mr-4 inline-block"
-                  />
-                  Sign In with GitHub
-                </button>
+                  <div className="flex h-screen flex-col overflow-scroll  bg-stone-900 p-8 text-white sm:h-fit sm:rounded-3xl sm:p-16">
+                    <div className="absolute left-4 top-4">
+                      <button
+                        onClick={toggleCurrentForm}
+                        className="transition-colors hover:text-amber-300 focus:text-amber-300"
+                      >
+                        <ChevronRightIcon className="h-10 rotate-180" />
+                      </button>
+                    </div>
+                    <div className="mb-8 text-4xl font-bold">Sign Up</div>
+                    <form className="flex flex-col gap-3">
+                      <label htmlFor="email" className="font-bold">
+                        Email
+                      </label>
+                      <TextBox
+                        id="email"
+                        name="email"
+                        placeholder="jonsnow@thewall.com"
+                        className="border border-stone-700 p-4 font-normal"
+                      />
+                      <label htmlFor="password" className="font-bold">
+                        Password
+                      </label>
+                      <TextBox
+                        type="password"
+                        id="password"
+                        name="password"
+                        className="border border-stone-700 p-4 font-normal"
+                      />
+                      <Button
+                        onClick={handleRegisterFormSubmit}
+                        className="my-5 bg-transparent p-4 text-amber-400 hover:text-amber-700 focus:text-amber-700"
+                      >
+                        Sign Up
+                      </Button>
+                    </form>
+                  </div>
+                </Transition>
               </div>
             </div>
           </div>
--- src/app/_components/icons/ChevronRightIcon.tsx
+++ src/app/_components/icons/ChevronRightIcon.tsx
@@ -2,8 +2,6 @@ export default function ChevronRightIcon(props: any) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       fill="none"
       viewBox="0 0 24 24"
       {...props}
